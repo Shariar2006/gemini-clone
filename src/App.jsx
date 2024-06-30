097565bbd681960ddd0ba07a8199a87bfc7e214d
Shariar2006 <shariarnafis86@gmail.com>
2024-06-30 18:44:18 +0600

    add favicon and title

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import MainLayOut from "./Components/MainLayOut/MainLayOut"
 import Sidebar from "./Components/Sidebar/Sidebar"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Sidebar/>
+      <MainLayOut/>
     </>
   )
 }
---
+++ src/Components/MainLayOut/MainLayOut.jsx
@@ -0,0 +1,12 @@
+
+const MainLayOut = () => {
+    return (
+        <div>
+            <div>
+                <h1>Gemini</h1>
+            </div>
+        </div>
+    );
+};
+
+export default MainLayOut;
