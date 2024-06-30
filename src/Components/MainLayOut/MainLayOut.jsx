fa7d6c10266c27d3d91849652ab8ad9c6bea41ec
Shariar2006 <shariarnafis86@gmail.com>
2024-06-30 18:54:13 +0600

    top bar done

--- src/Components/MainLayOut/MainLayOut.jsx
+++ src/Components/MainLayOut/MainLayOut.jsx
@@ -1,9 +1,11 @@
+import profile from "../../../public/profile.jpg"
 
 const MainLayOut = () => {
     return (
-        <div>
-            <div>
-                <h1>Gemini</h1>
+        <div className="flex-1 min-h-screen relative p-5">
+            <div className="flex justify-between items-center">
+                <h1 className="text-2xl text-[#4c4b4b]">Gemini</h1>
+                <img src={profile} className="w-14 h-14 rounded-full" alt="profile picture" />
             </div>
         </div>
     );
