77e566b5457d0ebc600953ad83a3a08cd40a1f79
Shariar2006 <shariarnafis86@gmail.com>
2024-08-03 20:06:49 +0600

    sent prompt and give result, show result in web page

--- src/Components/MainLayOut/MainLayOut.jsx
+++ src/Components/MainLayOut/MainLayOut.jsx
@@ -1,11 +1,33 @@
 import profile from "../../../public/profile.jpg"
+import icon from "../../../public/icon2.png"
 import { VscSend } from "react-icons/vsc";
 // import { MdOutlineKeyboardVoice } from "react-icons/md";
 // import { LuImagePlus } from "react-icons/lu";
 import "./MainLayOut.css"
 import Cards from "../Cards/Cards";
+import { useContext } from "react";
+import { Context } from "../../context/Context";
 
 const MainLayOut = () => {
+
+    const {
+        onSent,
+        prevPrompts,
+        setPrevPrompts,
+        recentPrompt,
+        setRecentPrompt,
+        showResult,
+        loading,
+        resultData,
+        setInput,
+        input
+    } = useContext(Context)
+
+    const handlePrompt = e => {
+        e.preventDefault()
+        onSent()
+    }
+
     return (
         <div className="flex-1 min-h-screen relative pt-5 pl-5 pr-5 ">
             <div className="md:flex justify-between items-center ml-60 md:ml-0">
@@ -14,20 +36,44 @@ const MainLayOut = () => {
             </div>
 
             <div className="max-w-4xl mx-auto pt-5 pl-5 pr-5">
-                <div className="welcomeText text-[25px] md:text-[56px]">
-                    <p><span>Hello, Shariar Alam</span></p>
-                    <p>How can I help you today?</p>
-                </div>
-                <Cards/>
+                {
+                    !showResult ? <div>
+                        <div className="welcomeText text-[25px] md:text-[56px]">
+                            <p><span>Hello, Shariar Alam</span></p>
+                            <p>How can I help you today?</p>
+                        </div>
+                        <Cards />
+                    </div> : <div className="h-[430px] result">
+
+                        <div className="flex items-center gap-5 mb-10">
+                            <img className="w-10 h-10 rounded-full" src={profile} alt="profile image" />
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="flex items-start gap-5">
+                            <img className="w-10 h-10 rounded-full" src={icon} alt="gemini icon" />
+                            {
+                                loading ? <div className="loader">
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                </div>
+                                    :
+                                    <p className="leading-loose" dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                            }
+                        </div>
+
+                    </div>
+                }
                 <div>
-                    <div className="relative mt-10">
-                        <input className="bg-[#F0F4F9] outline-none w-full p-5 rounded-full" type="text" placeholder="Enter a prompt here" />
+                    <form onSubmit={handlePrompt} className="relative mt-10">
+                        <input onChange={(e) => setInput(e.target.value)} name="prompt" value={input} className="bg-[#F0F4F9] outline-none w-full p-5 rounded-full" type="text" placeholder="Enter a prompt here" />
                         <div className="absolute flex gap-3 text-xl -mt-11 right-5">
                             {/* <LuImagePlus /> */}
                             {/* <MdOutlineKeyboardVoice /> */}
-                            <VscSend />
+                            <VscSend onClick={() => onSent()} className="cursor-pointer" />
+
                         </div>
-                    </div>
+                    </form>
                 </div>
                 <h1 className="text-sm text-center mt-5 text-gray-700">Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy & Gemini Apps</h1>
             </div>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,18 +1,50 @@
-import { createContext } from "react";
+/* eslint-disable react/prop-types */
+import { createContext, useState } from "react";
 import run from "../config/gemini";
 
 export const Context = createContext();
 
-const ContextProvider = ({children}) => {
+const ContextProvider = ({ children }) => {
+
+    const [input, setInput] = useState("")
+    const [recentPrompt, setRecentPrompt] = useState("")
+    const [prevPrompts, setPrevPrompts] = useState([])
+    const [showResult, setShowResult] = useState(false)
+    const [loading, setloading] = useState(false)
+    const [resultData, setResultData] = useState("")
 
     const onSent = async (prompt) => {
-        await run(prompt)
+        setResultData("")
+        setloading(true)
+        setShowResult(true)
+        setRecentPrompt(input)
+        const response = await run(input)
+        let responseArray = response.split("**");
+        let newResponse;
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i === 0 || i % 2 !== 1) {
+                newResponse += responseArray[i];
+            }else{
+                newResponse += "<b>"+responseArray[i]+"</b>";
+            }
+        }
+        let newResponse2 = newResponse.split("*").join('</br>');
+        setResultData(newResponse2)
+        setloading(false)
+        setInput("")
     }
 
-    onSent('what is react.js')
-
     const contextValue = {
-
+        onSent,
+        prevPrompts,
+        setPrevPrompts,
+        recentPrompt,
+        setRecentPrompt,
+        showResult,
+        loading,
+        resultData,
+        setInput,
+        input
     }
 
     return (
